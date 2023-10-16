6fca5398402ed585307ef303cb1b2e5b5c24370e
cchannawebdeveloper <cchanna.y.oeurng@gmail.com>
2023-10-16 17:53:35 +0700

    update

---
+++ src/main/java/com/example/awshelloworld/MainController.java
@@ -0,0 +1,21 @@
+package com.example.awshelloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class MainController {
+	
+	@GetMapping(value = {"/home","/"})
+	public String helloHome() {
+		return new String("Hello Home Page!!");
+	}
+	
+	@GetMapping(value = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
